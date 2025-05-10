9fd70787225b75028cc12ce33face62217a58bea
Pedro <pedserra.99@gmail.com>
2025-05-10 19:45:31 +0200

    todo arreglado

--- CRM-BACKEND/src/main/java/solution_market_CRM/service/EmployeeService.java
+++ CRM-BACKEND/src/main/java/solution_market_CRM/service/EmployeeService.java
@@ -1,20 +1,12 @@
 package solution_market_CRM.service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import solution_market_CRM.model.Employee;
 import solution_market_CRM.model.Employee;
 import solution_market_CRM.repository.EmployeeRepository;
 import solution_market_CRM.repository.SalesRepository;
